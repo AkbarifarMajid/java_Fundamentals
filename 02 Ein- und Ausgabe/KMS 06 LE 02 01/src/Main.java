e30fa6c3d3d2d0691e5a0a381668fc3111b3e27f
052240 <052240@ITET-ELAP-052>
2025-04-07 13:18:33 +0200

    Projekt KMS 06 LE 02 01 hinzugefügt

---
+++ 02 Ein- und Ausgabe/KMS 06 LE 02 01/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //Vorname abrufen
+        System.out.println("Biite geben Sie Ihre Name ein: ");
+        String vorname = scan.nextLine();
+
+        //Nachname Abrufen
+        System.out.println("Biite geben Sie Ihre Nachname ein: ");
+        String nachname = scan.nextLine();
+
+        //Info Anzeigen
+        System.out.printf("meine Name ist %s %s und ich habe java lernen angefangen ! \n", vorname , nachname );
+
+        //Scanner ausschlißen
+        scan.close();
+    }
+
+
+}
