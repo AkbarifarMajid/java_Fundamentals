6a64e141c67c84f5412260d65e30ec52c77bbaf2
052240 <052240@ITET-ELAP-052>
2025-04-09 09:55:54 +0200

    Nummerierung Programm mit kontrolle alle daten und print scritweiter zwischen

--- 03 Eingabe- Entscheidungen und Schleifen/KMS 06 LE 03 03/src/Nummerierung.java
+++ 03 Eingabe- Entscheidungen und Schleifen/KMS 06 LE 03 03/src/Nummerierung.java
@@ -1,12 +1,44 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Nummerierung {
     static Scanner myScanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    // _____________   Methode zur Überprüfung und Rückgabe einer Kommazahl (Preis) -----------
+    public static double readnummer(String text){
+        System.out.print(text);
 
+        while(true){
+            if (myScanner.hasNextDouble()) {
+                return myScanner.nextDouble();
+            } else {
+                System.out.println("FALSCH Eingabe!!");
+                myScanner.next();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("willkommen in Nummerierung Programm .");
+        double startNummer = readnummer("Bitte geben Sie Start Nummer: ");
+        double endNummer = readnummer("Bitte geben Sie End Nummer: ");
+        double stepNummer = readnummer("Bitte geben Sie die Schrittweite ein: ");
+
+        // Kontrolle: Wenn StarNummber > EndNummber ist
+        if(startNummer > endNummer){
+            System.out.println("!!!!!!!!!!!!!!!!!!");
+            System.out.println("Startnummer ist größer als Endnummer. Werte werden getauscht.");
+            double tempBox = startNummer;
+            startNummer = endNummer;
+            endNummer =tempBox;
+        }
+
+        System.out.println("Ihre einfache Nummerliste:");
+
+        double i = startNummer;
+        while(i <= endNummer){
+            System.out.printf("%.1f  ",i);
+            i += stepNummer;
+        }
 
     }//End main
+
 }//End Nummerierung
