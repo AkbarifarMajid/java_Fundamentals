37158fa06d3fd2051f15809d02e95adcb1f4cede
052240 <052240@ITET-ELAP-052>
2025-04-09 08:44:35 +0200

    Start Einfach Nummerieirung Programm

---
+++ 03 Eingabe- Entscheidungen und Schleifen/KMS 06 LE 03 03/Einfach Nummerieung/src/Nummerierung.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Nummerierung {
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+    }//End main
+}//End Nummerierung
