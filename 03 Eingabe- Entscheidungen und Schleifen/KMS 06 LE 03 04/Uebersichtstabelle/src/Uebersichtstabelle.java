9b35fef8ead7715dc1c9173fc4a932b73c6b263e
052240 <052240@ITET-ELAP-052>
2025-04-09 10:15:09 +0200

    Übersichtstabelle angefangen

---
+++ 03 Eingabe- Entscheidungen und Schleifen/KMS 06 LE 03 04/Uebersichtstabelle/src/Uebersichtstabelle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Uebersichtstabelle {
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("willkommen in Übersichtstabelle Programm .");
+
+
+    }//End main
+}// End Uebersichtstabelle
