8f6babe1b722aab30fdfdf8df6d0959d51f042e5
052240 <052240@ITET-ELAP-052>
2025-04-14 12:00:19 +0200

    Test comment

--- 06 Funktionen und Methoden/KMS 06 LE 06 01/MinundMaxInArrays/src/MinUndMaxInArrays.java
+++ 06 Funktionen und Methoden/KMS 06 LE 06 01/MinundMaxInArrays/src/MinUndMaxInArrays.java
@@ -4,6 +4,7 @@ public class MinUndMaxInArrays {
     static Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Wellcome bei Min und Max in Arrays");
 
 
     }//End main
