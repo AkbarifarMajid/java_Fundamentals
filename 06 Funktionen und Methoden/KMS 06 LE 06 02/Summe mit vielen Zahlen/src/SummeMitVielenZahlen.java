fed5d7fa9b9c4a754492b1825c33559d001025dc
052240 <052240@ITET-ELAP-052>
2025-04-14 14:47:50 +0200

    Summe mit Vielen Zahlen

---
+++ 06 Funktionen und Methoden/KMS 06 LE 06 02/Summe mit vielen Zahlen/src/SummeMitVielenZahlen.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SummeMitVielenZahlen {
+    static Scanner myScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+    }// End main
+
+}//End Summe mit Vielen Zahlen
