26491b8798f5eff8e1bf96402cd6ada15108d374
052240 <052240@ITET-ELAP-052>
2025-04-15 07:12:43 +0200

    Summe mit Vielen Zahlen mit varargs array

--- 06 Funktionen und Methoden/KMS 06 LE 06 02/Summe mit vielen Zahlen/src/SummeMitVielenZahlen.java
+++ 06 Funktionen und Methoden/KMS 06 LE 06 02/Summe mit vielen Zahlen/src/SummeMitVielenZahlen.java
@@ -1,9 +1,27 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class SummeMitVielenZahlen {
-    static Scanner myScanner = new Scanner(System.in);
-    public static void main(String[] args) {
+    //static Scanner myScanner = new Scanner(System.in);
 
+    // Diese Methode berechnet die Summe beliebig vieler Zahlen
+    public static double summeRechnen(double... beliebig) {
+        double summe = 0;
+        for (double zahl : beliebig) {
+            summe += zahl;
+        }
+        System.out.println("Anzahl: " + beliebig.length);
+        System.out.printf("Summe %.2f: ", summe);
+        return summe;
+    }
+
+//-------------------------------------
+    public static void main(String[] args) {
+        System.out.println("Berechnung der Summe:");
+        summeRechnen(100.5, 200, 50.25, 25, 2.2);
+        System.out.println("\n------------------------------");
+        summeRechnen(100.5, 200, 50.25, 25);
+        System.out.println("\n------------------------------");
+        summeRechnen(100.5, 200, 50.25);
 
     }// End main
 
