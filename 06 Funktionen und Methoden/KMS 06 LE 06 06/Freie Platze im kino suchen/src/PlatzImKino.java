8021225abd293b531274e4fafe43e2ff44a21191
052240 <052240@ITET-ELAP-052>
2025-04-16 12:42:28 +0200

    Start Program Freie plätze im Kino suchen

---
+++ 06 Funktionen und Methoden/KMS 06 LE 06 06/Freie Platze im kino suchen/src/PlatzImKino.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PlatzImKino {
+    static Scanner myScanner = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+
+    }// End main
+}// End Platz im Kino
