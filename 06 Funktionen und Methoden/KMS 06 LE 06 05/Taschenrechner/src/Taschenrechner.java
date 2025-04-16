a57e896115068c1a382e421f23d1562f17339fc0
052240 <052240@ITET-ELAP-052>
2025-04-16 08:39:54 +0200

    Start Project Taschenrechner

---
+++ 06 Funktionen und Methoden/KMS 06 LE 06 05/Taschenrechner/src/Taschenrechner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Taschenrechner {
+    static Scanner mySnanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+    }// End main
+}// End Taschenrechner
