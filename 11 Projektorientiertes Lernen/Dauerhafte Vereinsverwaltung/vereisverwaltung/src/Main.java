a44e73d5e21ea37a6d931e3156dcba2853055824
052240 <052240@ITET-ELAP-052>
2025-05-05 19:45:33 +0200

    connect to Database

--- 11 Projektorientiertes Lernen/Dauerhafte Vereinsverwaltung/vereisverwaltung/src/Main.java
+++ 11 Projektorientiertes Lernen/Dauerhafte Vereinsverwaltung/vereisverwaltung/src/Main.java
@@ -1,8 +1,22 @@
 import view.HauptmenueView;
 
+import database.DatabaseManager;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
 
-        new HauptmenueView().starten();
+        //new HauptmenueView().starten();
+        Connection con = DatabaseManager.getConnection();
+
+        if (con != null) {
+            System.out.println("✅ Verbindung zur Datenbank ist erfolgreich!");
+        } else {
+            System.out.println("❌ Verbindung fehlgeschlagen!");
+        }
+
+        // Verbindung am Ende schließen (optional)
+        DatabaseManager.verbindungSchliessen();
     }
 }
