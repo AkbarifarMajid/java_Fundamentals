264adcb15209acfa3c8db65aa32ffe425631b566
052240 <052240@ITET-ELAP-052>
2025-05-28 14:15:23 +0200

    fuhrpark+ GUI

--- 12 Grafisch Oberflächen/Fuhrpark_GUI/src/Main.java
+++ 12 Grafisch Oberflächen/Fuhrpark_GUI/src/Main.java
@@ -6,10 +6,10 @@ import gui.Main_Form;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            new Main_Form().setVisible(true);
+            //new Main_Form().setVisible(true);
 
 
-           //new Login_Form().setVisible(true);
+           new Login_Form().setVisible(true);
         });
     }
 }
